52086faf58b32ea85d5aa7a3f1005a247e2f6c5b
Hery <aeonzamaek@gmail.com>
2013-03-28 16:06:33 -0400

    working on auth

--- web.rb
+++ web.rb
@@ -11,16 +11,10 @@ use Rack::Session::Cookie
 use OmniAuth::Strategies::Shopify, ENV['SHOPIFY_KEY'], ENV['SHOPIFY_SECRET']
 
 get '/' do
-  <<-HTML
-  <ul>
-  <li><a href='/auth/shopify'>Sign in with Shopify</a></li>
-  </ul>
-  HTML
+  liquid :index
 end
 
 get '/auth/shopify/callback' do
   content_type 'text/plain'
-    url = request.env['omniauth.auth']['info']['urls']['site']
-      credentials = request.env['omniauth.auth']['credentials']
-        HTTParty.get("#{url}/orders.json?limit=5", :basic_auth => credentials, :headers => { 'ContentType' => 'application/json' }).inspect
-        end
+  request.env['omniauth.auth'].inspect
+end
