0658217d245675accf9f948f4b21d01908251f99
Hery <aeonzamaek@gmail.com>
2013-03-30 00:51:36 -0400

    completed authentication process

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
-source :rubygems
+source 'https://rubygems.org'
 gem 'sinatra', '1.1.0'
 gem 'liquid', '2.5.0'
-gem 'omniauth-shopify'
 gem 'httparty'
+gem 'shopify_api'
--- web.rb
+++ web.rb
@@ -4,17 +4,28 @@ require 'sinatra'
 require 'liquid'
 require 'net/http'
 require 'uri'
-require 'omniauth-shopify'
 require 'httparty'
+require 'json'
 
 use Rack::Session::Cookie
-use OmniAuth::Strategies::Shopify, ENV['SHOPIFY_KEY'], ENV['SHOPIFY_SECRET']
 
 get '/' do
   liquid :index
 end
 
-get '/auth/shopify/callback' do
-  content_type 'text/plain'
-  request.env['omniauth.auth'].inspect
+get '/auth' do
+  redirect "https://pandashop.myshopify.com/admin/oauth/authorize?client_id=8ba097d4d5e099c1deb61184b8a6dce5&scope=write_products&redirect_uri=http://localhost:5000/auth/callback"
 end
+
+get '/auth/callback' do
+  code = params[:code]
+  client_id = ENV['SHOPIFY_KEY']
+  client_secret= ENV['SHOPIFY_SECRET']
+  uri_string = "https://pandashop.myshopify.com/admin/oauth/access_token"
+  uri = URI(uri_string)
+  res = Net::HTTP.post_form(uri, 'client_id' => client_id, 'client_secret' => client_secret, 'code' => code)
+  json_res = JSON.parse(res.body)
+  permanent_token = json_res["access_token"]
+  p permanent_token 
+end
+
