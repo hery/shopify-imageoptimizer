57fd397fbbfbac6e7411edf3de56b2c7b43ed9be
Hery <aeonzamaek@gmail.com>
2013-03-31 20:58:40 -0400

    created forms for products update

--- web.rb
+++ web.rb
@@ -12,34 +12,45 @@ use Rack::Session::Cookie
 #get '/' do
 #  liquid :index
 #end
+#
+configure do 
+  set :shop_name, ''
+  set :client_id, ENV['SHOPIFY_KEY']
+  set :client_secret, ENV['SHOPIFY_SECRET']
+end
 
 get '/' do
-  # Initiate authentication
-  redirect "https://pandashop.myshopify.com/admin/oauth/authorize?"\
-           "client_id=8ba097d4d5e099c1deb61184b8a6dce5&"\
-           "scope=write_products&"\
-           "redirect_uri=http://localhost:5000/callback"
+  if (settings.shop_name == '')
+    liquid :index
+  else
+    # Initiate authentication
+    redirect "https://#{settings.shop_name}.myshopify.com/admin/oauth/authorize?"\
+             "client_id=#{settings.client_id}&"\
+             "scope=write_products&"\
+             "redirect_uri=http://localhost:5000/callback"
+  end
+end
+
+post '/' do
+  set :shop_name, params['shopName']
+  redirect "/"
 end
 
 get '/callback' do
   # Request to exchange temporary token
   code = params[:code]
-  client_id = ENV['SHOPIFY_KEY']
-  client_secret= ENV['SHOPIFY_SECRET']
-
-  permanent_token_request = HTTParty.post("https://pandashop.myshopify.com/"\
+  permanent_token_request = HTTParty.post("https://#{settings.shop_name}.myshopify.com/"\
                                           "admin/oauth/access_token",
-                      :query => { 'client_id' => client_id, 
-                                  'client_secret' => client_secret,
-                                  'code' => code 
-                                }
-                     )
+                      :query => { 'client_id' => settings.client_id, 
+                                  'client_secret' => settings.client_secret,
+                                  'code' => code })
 
   permanent_token_json_res = JSON.parse(permanent_token_request.body)                     
   permanent_token = permanent_token_json_res["access_token"]
 
   # Use permanent token to get products list
-  product_request = HTTParty.get("https://pandashop.myshopify.com/admin/products.json",
+  product_request = HTTParty.get("https://#{settings.shop_name}.myshopify.com/admin/"\
+                                  "products.json",
                                  :headers => { "X-Shopify-Access-Token" => permanent_token }
                                 )
   product_json = JSON.parse(product_request.body)
